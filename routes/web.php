5a52c8a178fa425ee8a9181ffbfabb21ca5d9001
cuishun <cuidaoji@outlook.com>
2020-01-09 16:25:45 +0800

    添加地址和控制器

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layouts.app');
-});
+})->middleware('verified');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
